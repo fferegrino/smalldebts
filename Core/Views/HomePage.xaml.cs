d585d3278c1a86599e3dd5ee8f9d97463392b718
Antonio Feregrino <antonio.feregrino@gmail.com>
2017-03-01 17:00:56 -0600

    Pull to refresh

--- Core/Views/HomePage.xaml.cs
+++ Core/Views/HomePage.xaml.cs
@@ -31,6 +31,8 @@ namespace Smalldebts.Core.UI.Views
         private SortingKind Sorting = SortingKind.ByAmount;
         private readonly SortingSettingsPage SortingSettingsPage;
 
+		public bool IsBusy { get; set; }
+
         public HomePage()
         {
             InitializeComponent();
@@ -59,6 +61,7 @@ namespace Smalldebts.Core.UI.Views
             tapSort.Tapped += async (sender, e) => await PopupNavigation.PushAsync(SortingSettingsPage);
             SortImage.GestureRecognizers.Add(tapSort);
 
+			DebtList.RefreshCommand = LoadDebtsCommand; 
 
             DebtList.ItemSelected += DebtList_ItemSelected;
 
@@ -73,6 +76,31 @@ namespace Smalldebts.Core.UI.Views
 
             MessagingCenter.Subscribe<DebtCell, DebtManipulationViewModel>(this, "deleted", Delete);
         }
+
+		private Command _loadDebtsCommand;
+
+		public Command LoadDebtsCommand
+		{
+			get
+			{
+				return _loadDebtsCommand ?? (_loadDebtsCommand = new Command(async () =>
+				{
+
+			if (IsBusy)
+						return;
+					IsBusy = true;
+					LoadDebtsCommand.ChangeCanExecute();
+					await LoadData();
+					IsBusy = false;
+					DebtList.IsRefreshing = false;
+					LoadDebtsCommand.ChangeCanExecute();
+				}, 
+				                                                             () =>
+			   {
+				   return !IsBusy;
+			   }));
+			}
+		}
 
 
         async Task LoadData()

04fcd2013dd9721d6d5e6b91a5d9bfd1b0b8a527
Antonio Feregrino <antonio.feregrino@gmail.com>
2017-03-02 23:52:37 -0600

    RemoveBorderEffect

---
+++ Core/Effects/RemoveBorderEffect.cs
@@ -0,0 +1,11 @@
+﻿using Xamarin.Forms;
+
+namespace Smalldebts.Core.UI.Effects
+{
+	public class RemoveBorderEffect : RoutingEffect
+	{
+		public RemoveBorderEffect() : base("Effects." + nameof(RemoveBorderEffect))
+		{
+		}
+	}
+}
---
+++ iOS/Effects/RemoveBorderEffect.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Smalldebts.iOS.Effects;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportEffect(typeof(RemoveBorderEffect), nameof(RemoveBorderEffect))]
+namespace Smalldebts.iOS.Effects
+{
+	public class RemoveBorderEffect : PlatformEffect
+	{
+		UITextBorderStyle old;
+
+		protected override void OnAttached()
+		{
+			var editText = Control as UITextField;
+			if (editText == null)
+				return;
+
+			old = editText.BorderStyle;
+			editText.BorderStyle = UITextBorderStyle.None;
+		}
+
+		protected override void OnDetached()
+		{
+			var editText = Control as UITextField;
+			if (editText == null)
+				return;
+
+			editText.BorderStyle = old;
+		}
+	}
+}

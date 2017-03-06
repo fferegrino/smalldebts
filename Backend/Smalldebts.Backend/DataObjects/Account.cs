e1da80ca4bc39317009a74341058ee6829e8f8f2
Antonio Feregrino <antonio.feregrino@gmail.com>
2017-03-06 15:35:40 -0600

    Revert "New projects"
    This reverts commit 841623e20db36e823340cf4880f465bd277f6617.

---
+++ Backend/Smalldebts.Backend/DataObjects/Account.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Azure.Mobile.Server;
+
+namespace Smalldebts.Backend.DataObjects
+{
+    public class Account : EntityData
+    {
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public byte[] Salt { get; set; }
+        public byte[] SaltedAndHashedPassword { get; set; }
+    }
+}
--- Smalldebts.WebSite/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Smalldebts.WebSite.MvcApplication" Language="C#" %>

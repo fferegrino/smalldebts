790884c5ed117cc6e27f459fc17b5c8028d83c6e
Antonio Feregrino <antonio.feregrino@gmail.com>
2017-03-06 15:35:14 -0600

    Revert "Bye bya core project"
    This reverts commit aa0ff8c2f908a1a4e74291753238d816cff20bcb.

---
+++ Smalldebts.Backend.Core/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Smalldebts.Backend.Core
+{
+    public class Class1
+    {
+    }
+}

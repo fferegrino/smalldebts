6fb25abdfca71fd2ac07a90ef3e231b89ff9e9a5
Antonio Feregrino <antonio.feregrino@gmail.com>
2017-03-13 10:38:11 -0600

    Mobile center integration

--- Core/App.xaml.cs
+++ Core/App.xaml.cs
@@ -1,5 +1,8 @@
 using System;
 using System.Threading.Tasks;
+using Microsoft.Azure.Mobile;
+using Microsoft.Azure.Mobile.Analytics;
+using Microsoft.Azure.Mobile.Crashes;
 using Microsoft.WindowsAzure.MobileServices;
 using Smalldebts.Core.UI.DataAccess;
 using Smalldebts.Core.UI.Services;
@@ -26,6 +29,13 @@ namespace Smalldebts.Core.UI
 		}
 
 		public static App RealCurrent => Current as App;
-        
+
+	    protected override void OnStart()
+	    {
+	        base.OnStart();
+            MobileCenter.Start("android=b4e7b97f-602a-483e-be47-10c3a406dcc3;" +
+                   "ios=92a01fb6-247b-4a42-9493-0776d643acb6;",
+                   typeof(Analytics), typeof(Crashes));
+        }
 	}
 }
